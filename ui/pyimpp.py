4660f48cd65160a3ee952631ec98acdedba612ea
Asaf R <asafro@gmail.com>
2016-02-20 18:40:28 -0800

    add impp UI

---
+++ ui/pyimpp.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import sys
+try:
+    import pygtk
+    pygtk.require("2.0")
+except:
+    pass
+try:
+    import gtk
+    import gtk.glade
+except:
+    sys.exit(1)
+
+class pyStages(object):
+  def __init__(self):
+    #Set the Glade file
+    self.gladefile = "pyimpp.glade"  
+    self.stagesTree = gtk.glade.XML(self.gladefile, "mainWindow") 
+      
+    #Create our dictionay and connect it
+    dic = {"on_mainWindow_destroy" : gtk.main_quit,
+           "on_tbAdd_clicked" : self.OnAddStage,
+           "on_tbClear_clicked" : self.OnClear,
+           }
+    self.stagesTree.signal_autoconnect(dic)
+    
+    #Here are some variables that can be reused later
+    self.cTemp = 0
+    self.cTime = 1
+    
+    self.sTemp = "Temperature (c)"
+    self.sTime = "Time (sec)"
+        
+    #Get the treeView from the widget Tree
+    self.stagesView = self.stagesTree.get_widget("stagesView")
+    #Add all of the List Columns to the stagesView
+    self.AddStagesListColumn(self.sTemp, self.cTemp)
+    self.AddStagesListColumn(self.sTime, self.cTime)
+  
+    #Create the listStore Model to use with the stagesView
+    self.stagesList = gtk.ListStore(str, str)
+    #Attache the model to the treeView
+    self.stagesView.set_model(self.stagesList)  
+    
+  def AddStagesListColumn(self, title, columnId):
+    column = gtk.TreeViewColumn(title, gtk.CellRendererText()
+      , text=columnId)
+    column.set_resizable(True)    
+    column.set_sort_column_id(columnId)
+    self.stagesView.append_column(column)
+    
+  def OnAddStage(self, widget):
+    temperatureText = self.stagesTree.get_widget("textTemperature")
+    temperature = temperatureText.get_text()
+
+    timeText = self.stagesTree.get_widget("textTime")
+    time = timeText.get_text()
+
+    if time and temperature:
+      self.stagesList.append(Stage(temperature, time).getList())
+
+  def OnClear(self, widget):
+    self.stagesList.clear()
+
+
+class Stage:
+  def __init__(self, temperature="", time=""):
+    self.temperature = temperature
+    self.time = time
+    
+  def getList(self):
+    return [self.temperature, self.time]    
+    
+if __name__ == "__main__":
+  stages = pyStages()
+  gtk.main()
